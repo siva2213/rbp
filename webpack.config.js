da494aad061cb88a72b73aa6aeb29f0baa2af35b
Siva Krishna Chettebhaktula <chsivakrishna1005@gmail.com>
2021-05-23 00:55:28 +0530

    folder structure modifications

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,28 @@
-const path = require('path')
+const path = require("path");
 module.exports = {
-    mode: 'development',
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'main.js'
-    },
-    target: 'node',
-    devServer: {
-        port: '3000',
-        contentBase: ['./public'],
-        open:true
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.json']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            }
-        ]
-    }
-}
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  target: "node",
+  devServer: {
+    port: "3000",
+    open: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
